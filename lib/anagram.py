c16aa9697f3a4f80f8964e4855978de487b52e11
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-05-25 20:03:32 +0300

    created anagram tester

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+  def __init__(self, word):
+      self.word = word
+
+  def match(self, list):
+      return [value for value in list if sorted(value) == sorted(self.word)]
+  
